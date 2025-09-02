67a3c0136d64387c588a19da6f6b39b7e34eec8c
Razafindrainibe Avotra Harilala Patrick <patrickmiavotra@gmail.com>
2025-09-02 12:57:13 +0200

    updat

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,40 @@
-import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.js'
-import { Provider } from 'react-redux'
-import { storeRedux } from './redux/store.tsx'
+// import React from 'react'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+// import App from './App.js'
+// import { Provider } from 'react-redux'
+// import { storeRedux } from './redux/store.tsx'
 
-createRoot(document.getElementById('root')!).render(
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <Provider store={storeRedux}>
+//       <BrowserRouter>
+//           <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </StrictMode>,
+// )
+
+// //npm i @reduxjs/toolkit react-redux
+// //npm i --save-dev @types/react-redux
+
+
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import { storeRedux } from './redux/store';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+
+createRoot(rootElement).render(
   <StrictMode>
     <Provider store={storeRedux}>
       <BrowserRouter>
-          <App />
+        <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>,
-)
-
-//npm i @reduxjs/toolkit react-redux
-//npm i --save-dev @types/react-redux
+  </StrictMode>
+);
